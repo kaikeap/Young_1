75e6ee9cb86adc8347b38679e32a3ee8dbffcd1b
kaikeap <kaikeapoliveira@gmail.com>
2025-04-26 17:23:10 -0300

    ex

---
+++ aula 12/main.py
@@ -0,0 +1,33 @@
+
+class Casa():
+
+    imobiliaria = ("Ctrl Imóveis")
+
+    def __init__(self, rua, bairro, CEP):
+        self.rua = rua
+        self.bairro = bairro
+        self.CEP = CEP
+
+def getImobiliaria(self):
+    return self.imobiliaria
+
+def getRua(self):
+    return self.rua
+
+def getImoBairro(self):
+    return self.bairro
+
+def getCEP(self):
+    return self.CEP
+
+def setImobiliaria(self, i):
+    self.imobiliaria = i
+
+def setRua(self, r):
+    self.imobiliaria = r
+
+def setBairro(self, b):
+    self.imobiliaria = b
+
+def setCEP(self, c):
+    self.imobiliaria = c
